225892df5721faeda86587da23848c4983b6db73
Amokrane Chentir <amokrane.chentir@blablacar.com>
2018-03-14 08:32:14 +0100

    Remove unnecessary dependencies

--- app/src/main/java/com/chentir/imageloader/demo/ImageAdapter.java
+++ app/src/main/java/com/chentir/imageloader/demo/ImageAdapter.java
@@ -39,8 +39,6 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         ImageLoader.getInstance(context).load(holder.imageView, dataSet[position]);
-        //Picasso.get().load(dataSet[position]).into(holder.imageView);
-        //Glide.with(context).load(dataSet[position]).into(holder.imageView);
     }
 
     @Override
